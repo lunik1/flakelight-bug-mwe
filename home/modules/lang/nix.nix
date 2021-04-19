7e27c0407c6f718467033944829e331ff290acee
lunik1 <ch.gpg@themaw.xyz>
2021-04-19 21:50:14 +0100

    Add nix prefetch tools

--- home/modules/lang/nix.nix
+++ home/modules/lang/nix.nix
@@ -1,5 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ nixfmt nixpkgs-fmt nix-linter nixpkgs-lint ];
+  home.packages = with pkgs; [
+    nix-linter
+    nix-prefetch
+    nix-prefetch-git
+    nix-prefetch-github
+    nixfmt
+    nixpkgs-fmt
+    nixpkgs-lint
+  ];
 }
