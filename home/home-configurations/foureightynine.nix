f1e67ffd8eb85afb2db1244860ca7ec6771c7eec
lunik1 <ch.gpg@themaw.xyz>
2021-06-13 16:41:51 +0100

    nixfmt

--- home/home-configurations/foureightynine.nix
+++ home/home-configurations/foureightynine.nix
@@ -1,49 +1,49 @@
 { inputs, overlays, ... }:
 
 inputs.home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-linux";
-          username = "corin";
-          homeDirectory = "/home/corin";
-          stateVersion = "20.09";
-          configuration = { pkgs, ... }: {
-            require = import ../modules/module-list.nix;
+  system = "x86_64-linux";
+  username = "corin";
+  homeDirectory = "/home/corin";
+  stateVersion = "20.09";
+  configuration = { pkgs, ... }: {
+    require = import modules/module-list.nix;
 
-            nixpkgs.config.allowUnfree = true;
-            nixpkgs.overlays = overlays;
+    nixpkgs.config.allowUnfree = true;
+    nixpkgs.overlays = overlays;
 
-            lunik1.home = {
-              waybar.batteryModule = true;
+    lunik1.home = {
+      waybar.batteryModule = true;
 
-              core.enable = true;
-              cli.enable = true;
-              gui.enable = true;
+      core.enable = true;
+      cli.enable = true;
+      gui.enable = true;
 
-              bluetooth.enable = true;
-              emacs.enable = true;
-              fonts.enable = true;
-              games.cli.enable = true;
-              git.enable = true;
-              gpg.enable = true;
-              megacmd.enable = true;
-              mpv.enable = true;
-              music.enable = true;
-              neovim.enable = true;
-              pulp-io.enable = true;
-              sway.enable = true;
-              syncthing.enable = true;
+      bluetooth.enable = true;
+      emacs.enable = true;
+      fonts.enable = true;
+      games.cli.enable = true;
+      git.enable = true;
+      gpg.enable = true;
+      megacmd.enable = true;
+      mpv.enable = true;
+      music.enable = true;
+      neovim.enable = true;
+      pulp-io.enable = true;
+      sway.enable = true;
+      syncthing.enable = true;
 
-              lang = {
-                c.enable = true;
-                clojure.enable = true;
-                data.enable = true;
-                julia.enable = true;
-                nix.enable = true;
-                prose.enable = true;
-                python.enable = true;
-                rust.enable = true;
-                sh.enable = true;
-                tex.enable = true;
-              };
-            };
-          };
-        }
+      lang = {
+        c.enable = true;
+        clojure.enable = true;
+        data.enable = true;
+        julia.enable = true;
+        nix.enable = true;
+        prose.enable = true;
+        python.enable = true;
+        rust.enable = true;
+        sh.enable = true;
+        tex.enable = true;
+      };
+    };
+  };
+}
