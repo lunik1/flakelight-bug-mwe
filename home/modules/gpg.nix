3a7d0216cb493ab0ef826a435b13358a35090ddc
lunik1 <ch.gpg@themaw.xyz>
2021-06-16 22:42:49 +0100

    gtk → gtk2

--- home/modules/gpg.nix
+++ home/modules/gpg.nix
@@ -21,7 +21,7 @@ in {
       enableSshSupport = true;
       defaultCacheTtl = 86400;
       maxCacheTtl = 86400;
-      pinentryFlavor = if config.lunik1.home.gui.enable then "gtk" else "curses";
+      pinentryFlavor = if config.lunik1.home.gui.enable then "gtk2" else "curses";
     };
   };
 }
