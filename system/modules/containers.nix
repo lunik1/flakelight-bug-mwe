f916a167eda24f0616dacf429e58fef46b748de6
lunik1 <ch.gpg@themaw.xyz>
2021-06-16 16:10:10 +0100

    Add containers module

---
+++ system/modules/containers.nix
@@ -0,0 +1,26 @@
+# Setup for ssh access
+
+{ config, lib, pkgs, ... }:
+
+let cfg = config.lunik1.system.containers;
+in {
+  options.lunik1.system.containers.enable =
+    lib.mkEnableOption "containerisation";
+
+  config = lib.mkIf cfg.enable {
+    environment = {
+      systemPackages = with pkgs; [ arion docker-client docker_compose ];
+      variables = {
+        COMPOSE_HTTP_TIMEOUT = "600";
+    PGID = "100";
+    PUID = "1000";
+      };
+    };
+
+    virtualisation.podman = {
+      enable = true;
+      dockerSocket.enable = true;
+      defaultNetwork.dnsname.enable = true;
+    };
+  };
+}
