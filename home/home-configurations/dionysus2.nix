2b36feaf518055ee39fc53f88305e413371bbc4b
lunik1 <ch.gpg@themaw.xyz>
2021-06-16 16:14:08 +0100

    Add dionysus2 flake output

---
+++ home/home-configurations/dionysus2.nix
@@ -0,0 +1,38 @@
+{ inputs, overlays, ... }:
+
+inputs.home-manager.lib.homeManagerConfiguration {
+  system = "x86_64-linux";
+  username = "corin";
+  homeDirectory = "/home/corin";
+  stateVersion = "21.11";
+
+  configuration = { pkgs, ... }: {
+    require = import ../modules/module-list.nix;
+
+    nixpkgs.config.allowUnfree = true;
+
+    # Use ffmpeg build with nonfree components
+    nixpkgs.overlays = overlays ++ [
+      (self: super: {
+        ffmpeg-full = super.ffmpeg-full.override {
+          ffplayProgram = false;
+          runtimeCpuDetectBuild = false; # compile natively
+          nonfreeLicensing = true;
+          enableLto = true;
+        };
+      })
+    ];
+
+    lunik1.home = {
+      core.enable = true;
+      cli.enable = true;
+
+      emacs.enable = true;
+      git.enable = true;
+      gpg.enable = true;
+      neovim.enable = true;
+
+      lang.nix.enable = true;
+    };
+  };
+}
--- system/flake.nix
+++ system/flake.nix
@@ -2,9 +2,15 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05-small";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.foureightynine = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ (import systems/foureightynine.nix) ];
+    nixosConfigurations = {
+      foureightynine = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ (import systems/foureightynine.nix) ];
+      };
+      dionysus2 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ (import systems/dionysus2.nix) ];
+      };
     };
   };
 }
