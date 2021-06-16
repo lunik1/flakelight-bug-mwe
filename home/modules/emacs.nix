1424cfbdf4d47ba5b4ce4983109676d6fc3dd49c
lunik1 <ch.gpg@themaw.xyz>
2021-06-16 22:01:17 +0100

    Only enable gui emacs features if gui module enabled

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -7,24 +7,22 @@ in {
   config = lib.mkIf cfg.enable {
     lunik1.home.git.enable = true;
 
-    home.packages = with pkgs; [
-      fd
-      glslang
-      gnuplot
-      graphviz
-      pandoc
-      ripgrep
-      sqlite.bin
-
-      emacs-all-the-icons-fonts
-      myosevka
-      myosevka-aile
-      myosevka-etoile
-    ];
+    home.packages = with pkgs;
+      [ fd glslang gnuplot graphviz pandoc ripgrep sqlite.bin ]
+      ++ lib.optionals config.lunik1.home.gui.enable [
+        emacs-all-the-icons-fonts
+        myosevka
+        myosevka-aile
+        myosevka-etoile
+      ];
 
     programs.emacs = {
       enable = true;
-      extraPackages = epkgs: [ epkgs.vterm epkgs.pdf-tools ];
+      package = with pkgs;
+        if config.lunik1.home.gui.enable then emacs else emacs-nox;
+      extraPackages = epkgs:
+        [ epkgs.vterm ]
+        ++ lib.optionals config.lunik1.home.gui.enable [ epkgs.pdf-tools ];
     };
 
     services.gpg-agent.extraConfig = ''
