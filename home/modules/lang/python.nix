daa2a03a47b42fa8360e9d2dc542abd33d0dcd56
lunik1 <ch.gpg@themaw.xyz>
2021-07-23 18:44:15 +0100

    (lang.python) Replace python language server with pyright

--- home/modules/lang/python.nix
+++ home/modules/lang/python.nix
@@ -5,6 +5,6 @@ in {
   options.lunik1.home.lang.python.enable = lib.mkEnableOption "Python";
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [ black poetry python-language-server python3 ];
+    home.packages = with pkgs; [ black poetry nodePackages.pyright python3 ];
   };
 }
