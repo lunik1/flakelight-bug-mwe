90a8af8b92785a44686a9655c93d4ca7d7a8de62
lunik1 <ch.gpg@themaw.xyz>
2022-02-16 17:36:47 +0000

    Re-enable neogit

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -86,13 +86,12 @@ in {
           plugin = nvim-colorizer-lua;
           config = luaWrap "require('colorizer').setup()";
         }
-        # Causing crash on startup
-        # {
-        #   plugin = neogit;
-        #   config = ''
-        #     lua require('neogit').setup()
-        #   '';
-        # }
+        {
+          plugin = neogit;
+          config = ''
+            lua require('neogit').setup()
+          '';
+        }
         {
           plugin = nvim-cmp;
           config =
