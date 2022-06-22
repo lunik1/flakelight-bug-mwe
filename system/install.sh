3b2fd6f3a2e6fe367409ab3e708b5b0618a60195
lunik1 <ch.gpg@themaw.xyz>
2022-06-22 09:48:18 +0100

    Copy system configuration to /etc/nixos
    nix will now refuse to build from a link

--- system/install.sh
+++ system/install.sh
@@ -5,5 +5,5 @@ set -Eeuo pipefail
 # dir of this script
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-sudo ln --symbolic --no-dereference --force "${DIR}" /etc/nixos
+sudo cp -r "${DIR}" /etc/nixos
 sudo nixos-rebuild switch --flake "/etc/nixos#${1}"
