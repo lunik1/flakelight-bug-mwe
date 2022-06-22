ac39d0bc618a351384ca3918f48c6b68ca35d648
lunik1 <ch.gpg@themaw.xyz>
2022-06-22 11:33:19 +0100

    Remove unused ntfs utils

--- system/systems/hermes.nix
+++ system/systems/hermes.nix
@@ -13,9 +13,6 @@ in {
     system.stateVersion = "21.05";
     nix.maxJobs = 8;
 
-    ## Add ability to muotn nfs
-    # environment.systemPackages = with pkgs; [ nfsUtils ];
-
     fileSystems."/mnt/nas" = {
       device = "172.16.129.119:/nas/4657";
       fsType = "nfs";
