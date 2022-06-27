e0ffc7ff2ae9727196ba18f923dc0f49e1e6f0a3
lunik1 <ch.gpg@themaw.xyz>
2022-06-27 21:54:03 +0100

    Delete /etc/nixos before replacing it

--- system/install.sh
+++ system/install.sh
@@ -5,5 +5,6 @@ set -Eeuo pipefail
 # dir of this script
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+sudo rm -rf /etc/nixos
 sudo cp -r "${DIR}" /etc/nixos
 sudo nixos-rebuild switch --flake "/etc/nixos#${1}"
