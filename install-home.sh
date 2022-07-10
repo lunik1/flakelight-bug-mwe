e893aa2e10ce5da3c6e26d8b0ae9ac480eb48144
lunik1 <ch.gpg@themaw.xyz>
2022-07-10 18:54:47 +0100

    Add workaround for nix-community/home-manager#2848

--- install-home.sh
+++ install-home.sh
@@ -11,6 +11,9 @@ ln --symbolic --no-dereference --force "${DIR}" "${HOME}/.config"
 nix-shell -p nixFlakes --command \
   "nix build -o ${DIR}/result --experimental-features 'nix-command flakes' '${DIR}#homeConfigurations.${1}'"
 
+# remove old home manager profiles b/c of https://github.com/nix-community/home-manager/issues/2848
+nix profile list | awk '$4 ~ "/nix/store/[0-9a-z]{32}-home-manager-path" { print $1 }' | xargs -r nix profile remove
+
 # Activate new environment
 # shellcheck disable=SC1091
 . "${DIR}/result/activate"
