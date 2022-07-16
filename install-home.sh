de070c19f4f34be452bbca0ef8ac287283751c25
lunik1 <ch.gpg@themaw.xyz>
2022-07-16 02:36:57 +0100

    Use current system's hostname as default argument to install scripts

--- install-home.sh
+++ install-home.sh
@@ -5,10 +5,22 @@ set -Eeuo pipefail
 # dir of this script
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+if [[ ${1+x} ]]; then
+  HOST=${1}
+else
+  if [[ ${HOSTNAME+x} ]]; then
+    printf "Hostname not supplied, using '%s'\n" "${HOSTNAME}"
+    HOST=${HOSTNAME}
+  else
+    printf "Failed to determine hostname, aborting\n"
+    exit 1
+  fi
+fi
+
 # Build environment
 mkdir -p "${HOME}/.config"
 ln --symbolic --no-dereference --force "${DIR}" "${HOME}/.config"
-nix --experimental-features 'nix-command flakes' build -o "${DIR}/result" "${DIR}#homeConfigurations.${1}"
+nix --experimental-features 'nix-command flakes' build -o "${DIR}/result" "${DIR}#homeConfigurations.${HOST}"
 
 # remove old home manager profiles b/c of https://github.com/nix-community/home-manager/issues/2848
 nix --experimental-features 'nix-command flakes' profile list | awk '$4 ~ "/nix/store/[0-9a-z]{32}-home-manager-path" { print $1 }' | xargs -r nix profile remove
--- install-system.sh
+++ install-system.sh
@@ -5,6 +5,18 @@ set -Eeuo pipefail
 # dir of this script
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+if [[ ${1+x} ]]; then
+  HOST=${1}
+else
+  if [[ ${HOSTNAME+x} ]]; then
+    printf "Hostname not supplied, using '%s'\n" "${HOSTNAME}"
+    HOST=${HOSTNAME}
+  else
+    printf "Failed to determine hostname, aborting\n"
+    exit 1
+  fi
+fi
+
 sudo rm -rf /etc/nixos
 sudo cp -r "${DIR}" /etc/nixos
-sudo nixos-rebuild switch --flake "/etc/nixos#${1}"
+sudo nixos-rebuild switch --flake "/etc/nixos#${HOST}"
