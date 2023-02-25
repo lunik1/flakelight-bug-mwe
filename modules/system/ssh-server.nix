2a0a72345e5702cf7288c3b6db5877f49b964091
lunik1 <ch.gpg@themaw.xyz>
2023-02-25 15:10:34 +0000

    ssh: cleanup authorised keys

--- modules/system/ssh-server.nix
+++ modules/system/ssh-server.nix
@@ -13,7 +13,6 @@ in {
         enable = true;
         hostKeys = [ /etc/secrets/initrd/ssh_host_ed25519_key ];
         authorizedKeys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID17KStyIU+A23dghTW/6a/sR71Za8prKJcAkZrrhbJm corin@foureightynine"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmX7eYDHycsNfGU3x9Yzh6Cx7xd1JMhj2ca3cg8ELOK openpgp:0xE480A7A9"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkSRk4VCfwnoNBH/dT5F3mRbYV9U9yt6NNb6XpbVTan openpgp:0x2559C602"
         ];
@@ -36,10 +35,6 @@ in {
     };
 
     users.users.corin.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0+cUVaESw5u7V/S7tAmKSYE0u0Ij7eOsxH2rgzGXLH corin@thesus"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMzMGelXQHdhXbxlTJ+DzW+b8Lojkawr7+9JmftgzeCI corin@dionysus2"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID17KStyIU+A23dghTW/6a/sR71Za8prKJcAkZrrhbJm corin@foureightynine"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1djimfe24mF+GRzwWlYULJtmJ7hbiMvSoel0tR4ZFd corin@hermes"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmX7eYDHycsNfGU3x9Yzh6Cx7xd1JMhj2ca3cg8ELOK openpgp:0xE480A7A9"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkSRk4VCfwnoNBH/dT5F3mRbYV9U9yt6NNb6XpbVTan openpgp:0x2559C602"
     ];
