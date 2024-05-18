7070a10260a98d3deb3ce7ad55d62ea9f2fd1078
lunik1 <ch.gpg@themaw.xyz>
2024-05-18 13:24:35 +0100

    gnome: remove VRR patches
    Now upstream

--- flake.nix
+++ flake.nix
@@ -53,26 +53,6 @@
         };
 
         withOverlays = [
-          (self: super: {
-            gnome = super.gnome.overrideScope (gnomeFinal: gnomePrev: {
-              mutter = gnomePrev.mutter.overrideAttrs (old: {
-                patches = (old.patches or [ ]) ++ [
-                  (super.fetchpatch {
-                    url = "https://gist.githubusercontent.com/lunik1/3428ca679d5c7c0bd3b791f8b4a605c4/raw/234f3717047e8d90fc2d386192526bac1ee54c98/mutter-vrr.patch";
-                    sha256 = "sha256-YpX+DK7BvHAnVSfOvtudb3bpMcU6bsNw8PI+b4hj/eU=";
-                  })
-                ];
-              });
-              gnome-control-center = gnomePrev.gnome-control-center.overrideAttrs (old: {
-                patches = (old.patches or [ ]) ++ [
-                  (super.fetchpatch {
-                    url = "https://gist.githubusercontent.com/lunik1/43d5d6b114084f087cb248aafea75f2e/raw/26e5c37df85ddf75f2070eafcfc42865b2a164f8/gnome-control-ceter-vrr.patch";
-                    sha256 = "sha256-QSi3KlNA2DSMp7B8lpval+nlCqREK9Ch1Kj2kaGq+QM=";
-                  })
-                ];
-              });
-            });
-          })
           (self: super: { lunik1-nur = import inputs.lunik1-nur { pkgs = super; }; })
           (self: super: { nix-wallpaper = super.inputs'.nix-wallpaper.packages.default; })
           (self: super: { yt-dlp = super.yt-dlp.override { withAlias = true; }; })
