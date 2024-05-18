97f675bf24a5839121a0e2e87b9a6ca2aea92466
lunik1 <ch.gpg@themaw.xyz>
2024-05-18 13:24:04 +0100

    remove NUR inadyn module
    Now in nixpkgs

--- nix/nixosConfigurations/default.nix
+++ nix/nixosConfigurations/default.nix
@@ -4,7 +4,6 @@ let
   addCommonCfg = prev: prev // {
     modules = prev.modules ++ (with inputs; [
       sops-nix.nixosModules.sops
-      lunik1-nur.nixosModules.inadyn
     ]) ++ lib.attrValues outputs.nixosModules;
   };
 in
