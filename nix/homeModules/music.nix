caba1fe8480058c75a4510660cd508f909d2c2cc
lunik1 <ch.gpg@themaw.xyz>
2024-05-18 13:23:37 +0100

    music: use feishin from nixpkgs

--- nix/homeModules/music.nix
+++ nix/homeModules/music.nix
@@ -7,10 +7,9 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs;
       [
+        feishin
         playerctl
         spotify
-
-        lunik1-nur.feishin-appimage
       ]
       ++ lib.optional config.lunik1.home.gui.enable spotify;
     programs = {
