6f4c9649c977ca6c3d8bff11a109a1d16e0120f9
lunik1 <ch.gpg@themaw.xyz>
2024-06-21 22:31:29 +0100

    neovim: restore diagnostic markers in signs column

--- nix/homeModules/neovim.nix
+++ nix/homeModules/neovim.nix
@@ -1349,7 +1349,6 @@ in
 
             vim.diagnostic.config({
               virtual_text = false,
-              signs = false,
               underline = true,
               update_in_insert = false,
               severity_sort = true,
