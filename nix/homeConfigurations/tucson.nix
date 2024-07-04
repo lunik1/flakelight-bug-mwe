8f3b02512dbf9303eb50aa8819a9fe9a42e4231f
lunik1 <ch.gpg@themaw.xyz>
2024-07-04 19:24:06 +0100

    tucson: enable clojure

--- nix/homeConfigurations/tucson.nix
+++ nix/homeConfigurations/tucson.nix
@@ -51,6 +51,7 @@
 
           lang = {
             c.enable = true;
+            clojure.enable = true;
             data.enable = true;
             julia.enable = true;
             nix.enable = true;
