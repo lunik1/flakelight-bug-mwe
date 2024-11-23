489cdcd6fd10684f2cb25b5e3f8f083f9c7ab6e6
lunik1 <ch.gpg@themaw.xyz>
2024-11-23 01:37:15 +0000

    system.games: add gamemode

--- nix/nixosModules/games.nix
+++ nix/nixosModules/games.nix
@@ -10,17 +10,40 @@ let
 in
 {
   options.lunik1.system.games = {
+    enable = lib.mkEnableOption "games";
     steam.enable = lib.mkEnableOption "steam";
   };
 
-  config = {
-    programs.steam = lib.mkIf cfg.steam.enable {
-      enable = true;
-      extraCompatPackages = with pkgs; [ proton-ge-bin ];
-      gamescopeSession.enable = true;
-      localNetworkGameTransfers.openFirewall = true;
-      protontricks.enable = true;
-      remotePlay.openFirewall = true;
+  config = lib.mkIf cfg.enable {
+    programs = {
+      gamemode = {
+        enable = true;
+        enableRenice = true;
+        settings = {
+          general = {
+            desiredgov = "performance";
+            renice = 5;
+            softrealtime = "auto";
+          };
+
+          gpu = {
+            apply_gpu_optimisations = "accept-responsibility";
+            gpu_device = 1;
+            amd_performance_level = "high";
+          };
+        };
+      };
+
+      steam = lib.mkIf cfg.steam.enable {
+        enable = true;
+        extraCompatPackages = with pkgs; [ proton-ge-bin ];
+        gamescopeSession.enable = true;
+        localNetworkGameTransfers.openFirewall = true;
+        protontricks.enable = true;
+        remotePlay.openFirewall = true;
+      };
     };
+
+    users.users.corin.extraGroups = [ "gamemode" ];
   };
 }
