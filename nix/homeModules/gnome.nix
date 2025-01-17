a67213f456d8224093887f1a40d90135b070180b
lunik1 <ch.gpg@themaw.xyz>
2025-01-17 21:21:04 +0000

    home.gnome: use ghostty home manager module

--- nix/homeModules/gnome.nix
+++ nix/homeModules/gnome.nix
@@ -23,7 +23,6 @@ in
 
     home.packages = with pkgs; [
       mission-center
-      ghostty
     ];
 
     programs = {
@@ -31,6 +30,32 @@ in
         profiles.corin.settings = {
           "mousewheel.min_line_scroll_amount" = 70;
         };
+      };
+      ghostty = {
+        enable = true;
+        enableZshIntegration = true;
+        installBatSyntax = true;
+        installVimSyntax = true;
+        settings =
+          {
+            font-family = [
+              "Myosevka"
+              "JuliaMono"
+            ];
+            font-codepoint-map = "U+E000-U+E00A,U+EA60-U+EBEB,U+E0A0-U+E0C8,U+E0CA,U+E0CC-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6B1,U+E700-U+E7C5,U+F000-U+F2E0,U+F300-U+F372,U+F400-U+F532,U+F0001-U+F1AF0=Symbols Nerd Font Mono";
+            font-size = 12;
+            font-thicken = true;
+            gtk-single-instance = true;
+            shell-integration = "zsh";
+            theme = "GruvboxDark";
+          }
+          // lib.optionalAttrs pkgs.stdenv.isDarwin {
+            macos-icon = "custom-style";
+            macos-icon-frame = "beige";
+            macos-icon-ghost-color = "#EBDBB2";
+            macos-option-as-alt = "left";
+            macos-titlebar-style = "native";
+          };
       };
       kitty = lib.mkMerge [
         ((import ../../config/kitty/kitty.nix) { inherit config lib pkgs; })
@@ -48,21 +73,6 @@ in
       "autostart/gnome-keyring-ssh.desktop".text = ''
         ${lib.fileContents "${pkgs.gnome-keyring}/etc/xdg/autostart/gnome-keyring-ssh.desktop"}
           Hidden=true
-      '';
-      "ghostty/config".text = ''
-        font-family = Myosevka
-        font-family = JuliaMono
-        font-codepoint-map = U+E000-U+E00A,U+EA60-U+EBEB,U+E0A0-U+E0C8,U+E0CA,U+E0CC-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6B1,U+E700-U+E7C5,U+F000-U+F2E0,U+F300-U+F372,U+F400-U+F532,U+F0001-U+F1AF0=Symbols Nerd Font Mono
-        font-size = 12
-        font-thicken = true
-        gtk-single-instance = true
-        macos-icon = custom-style
-        macos-icon-frame = beige
-        macos-icon-ghost-color = #EBDBB2
-        macos-option-as-alt = left
-        macos-titlebar-style = native
-        shell-integration = zsh
-        theme = GruvboxDark
       '';
     };
   };
