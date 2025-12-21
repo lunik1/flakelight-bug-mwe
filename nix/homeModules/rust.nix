aca9a6daa34793054147c9e4331863d570a809ab
lunik1 <ch.gpg@themaw.xyz>
2025-12-21 17:13:29 +0000

    home.rust: use cargo module

--- nix/homeModules/rust.nix
+++ nix/homeModules/rust.nix
@@ -12,16 +12,15 @@ in
   options.lunik1.home.lang.rust.enable = lib.mkEnableOption "rust";
 
   config = lib.mkIf cfg.enable {
-    home = {
-      sessionVariables = {
-        CARGO_REGISTRIES_CRATES_IO_PROTOCOL = "sparse";
-      };
-
-      packages = with pkgs; [ cargo ];
+    home.sessionVariables = {
+      CARGO_REGISTRIES_CRATES_IO_PROTOCOL = "sparse";
     };
 
-    programs.zsh.envExtra = ''
-      export PATH=$HOME/bin:$HOME/.cargo/bin/:$PATH
-    '';
+    programs = {
+      cargo.enable = true;
+      zsh.envExtra = ''
+        export PATH=$HOME/bin:$HOME/.cargo/bin/:$PATH
+      '';
+    };
   };
 }
