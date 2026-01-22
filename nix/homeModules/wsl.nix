0b5edc412c124dcbf4db432534c999af35b6b4f4
lunik1 <ch.gpg@themaw.xyz>
2026-01-22 23:40:25 +0000

    home.wsl: add wsl-open

--- nix/homeModules/wsl.nix
+++ nix/homeModules/wsl.nix
@@ -14,7 +14,12 @@ in
   options.lunik1.home.wsl.enable = lib.mkEnableOption "settings for WSL systems";
 
   config = lib.mkIf cfg.enable {
-    home.sessionVariables.LIBGL_ALWAYS_INDIRECT = 1;
+    home = {
+      packages = with pkgs; [
+        wsl-open
+      ];
+      sessionVariables.LIBGL_ALWAYS_INDIRECT = 1;
+    };
 
     programs.zsh.envExtra = lib.mkIf config.programs.zsh.enable ''
       export DISPLAY=$(${pkgs.gawk}/bin/awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
