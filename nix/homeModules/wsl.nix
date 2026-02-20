0560b2b7e44d910a5dd07e762ef4b95e0c49da64
lunik1 <ch.gpg@themaw.xyz>
2026-02-20 11:17:42 +0000

    home.wsl: encourage x11/vxcsrv over WSLg

--- nix/homeModules/wsl.nix
+++ nix/homeModules/wsl.nix
@@ -23,8 +23,27 @@ in
         LIBGL_ALWAYS_SOFTWARE = "true";
       };
     };
-    programs.zsh.shellAliases = {
-      open = "wsl-open";
+    programs.zsh = {
+      shellAliases = {
+        open = "wsl-open";
+      };
+
+      profileExtra = ''
+        ${lib.getExe pkgs.setxkbmap} -option compose:ralt &> /dev/null
+      '';
+
+      # avoid WSLg: pretty buggy in my experience and windows don't integrate well
+      # can't disable it entirely as it breaks docker integration
+      envExtra = ''
+        export DISPLAY=$(${lib.getExe pkgs.busybox} ip route | ${lib.getExe pkgs.busybox} awk '/default/ {print $3}'):0.0
+        export GDK_BACKEND=x11
+        export QT_QPA_PLATFORM=xcb
+        export SDL_VIDEODRIVER=x11
+        # export XDG_RUNTIME_DIR=/tmp/no-wslg
+
+        unset PULSE_SERVER
+        unset WAYLAND_DISPLAY
+      '';
     };
   };
 }
